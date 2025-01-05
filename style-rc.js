7bbce341338dba81e21f0bfdb44c052c5a8302d2
Remy Telford-Smith <remy.ts@me.com>
2025-01-05 17:39:08 +0000

    added countdown timer on all rc's bug= fridge layout wont stay in a table formation

--- style-rc.js
+++ style-rc.js
@@ -1,3 +1,34 @@
 setTimeout(() => {
-    window.location.href = "next_page.html"; 
+    window.location.href = "fridge.html"; 
   }, 5000); 
+
+  //recipe cards clickability
+const recipeCards = document.querySelectorAll('.recipe-card');
+
+recipeCards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('active'); 
+  });
+});
+
+//start timers and make them move
+const timerContainer = document.querySelector('.timer-container');
+    const timerText = document.querySelector('.timer-text');
+
+    function startTimer() {
+      let timeLeft = 5;
+
+      const timerInterval = setInterval(() => {
+        timerText.textContent = timeLeft; 
+
+        if (timeLeft <= 0) {
+          clearInterval(timerInterval); 
+          window.location.href = 'pizza-rc.html', 'salade-rc.html', 'prawn-rc.html', 'patatas-rc.html'; 
+        }
+
+        timeLeft--; // Decrement the time
+      }, 1000); // Update every 1 second
+    }
+
+    // Start the timer automatically when the page loads
+    startTimer(); 
