2534b14a8d60d8c77e03f2dccf6b49fba3215787
Remyvts11 <vrjs6kz6@students.codeinstitute.net>
2025-03-18 15:34:26 +0000

    debugged the recipeingredientss not showing when cards are clicked to start game- too may event handlers

--- style.js
+++ style.js
@@ -1,22 +1,3 @@
-// Function to handle recipe card clicks
-//function startGame(recipeName) {
-  //localStorage.setItem("selectedRecipe", recipeName);
-  //window.location.href = "fridge.html";
- //}
- 
- // Get all recipe card elements
- //const recipeCards = document.querySelectorAll('.recipe-card');
- 
- // Add click event listeners to each recipe card
- //recipeCards.forEach(card => {
-  //card.addEventListener('click', () => {
-    //recipeCards.forEach(card => card.classList.remove('active')); // Remove 'active' class from all cards
-    //card.classList.add('active'); // Add 'active' class to the clicked card
-  //  const recipeName = card.querySelector('h3').textContent.replace(' Recipe', '').toLowerCase();
-   // startGame(recipeName);
- // });
-// });
-
 //store recipes in local storage
   const recipes = {
     prawn: ["prawns", "olive oil"],
@@ -28,7 +9,5 @@
     localStorage.setItem('selectedRecipe', JSON.stringify(recipes[recipeName]));
     location.href = 'fridge.html';
   }
- // localStorage.setItem('selectedRecipe', JSON.stringify(recipes[recipeCards]));
-  //location.href = 'fridge.html';
-
+ 
 
